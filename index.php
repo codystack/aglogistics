a953490d85db70053cdb025a6e2a010138794fd3
codystack <ebuzzadvert@gmail.com>
2020-08-17 08:33:08 +0100

    new ui add

---
+++ index.php
@@ -0,0 +1,234 @@
+<?php
+include ('./components/header.php');
+?>
+
+<main>
+    <!--? slider Area Start-->
+    <div class="slider-area ">
+        <div class="slider-active">
+            <!-- Single Slider -->
+            <div class="single-slider slider-height d-flex align-items-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-9 col-lg-9">
+                            <div class="hero__caption">
+                                <h1 style="font-size: 75px">Safe & Reliable <span>Logistic</span> Solutions!</h1>
+                            </div>
+                            <!--Hero form -->
+                            <form action="#" class="search-box">
+                                <div class="input-form">
+                                    <input type="text" placeholder="Your Tracking ID">
+                                </div>
+                                <div class="search-form">
+                                    <a href="#">Track & Trace</a>
+                                </div>	
+                            </form>	
+                            <!-- Hero Pera -->
+                            <div class="hero-pera">
+                                <p>For order status inquiry</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- slider Area End-->
+    <!--? Categories Area Start -->
+    <div class="categories-area section-padding30">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <!-- Section Tittle -->
+                    <div class="section-tittle text-center mb-80">
+                        <span>Our Services</span>
+                        <h2>What We Can Do For You</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="single-cat text-center mb-50">
+                        <div class="cat-icon">
+                            <span class="flaticon-shipped"></span>
+                        </div>
+                        <div class="cat-cap">
+                            <h5><a href="services.html">Domestic Courier</a></h5>
+                            <p>The sea freight service has grown conside rably in recent years. We spend timetting to know your processes to.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="single-cat text-center mb-50">
+                        <div class="cat-icon">
+                            <span class="flaticon-ship"></span>
+                        </div>
+                        <div class="cat-cap">
+                            <h5><a href="services.html">e-Commerce Courier</a></h5>
+                            <p>The sea freight service has grown conside rably in recent years. We spend timetting to know your processes to.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="single-cat text-center mb-50">
+                        <div class="cat-icon">
+                            <span class="flaticon-shipped"></span>
+                        </div>
+                        <div class="cat-cap">
+                            <h5><a href="services.html">Inter-state Courier</a></h5>
+                            <p>We reckon how important it is for you to reach all your national destinations via a single service provider with capacity to respond flexibly.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Categories Area End -->
+    <!--? About Area Start -->
+    <div class="about-low-area padding-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-12">
+                    <div class="about-caption mb-50">
+                        <!-- Section Tittle -->
+                        <div class="section-tittle mb-35">
+                            <span>About Our Company</span>
+                            <h2>Safe Logistic & Transport  Solutions That Saves our Valuable Time!</h2>
+                        </div>
+                        <p>Brook presents your services with flexible, convenient and cdpose layouts. You can select your favorite layouts & elements for cular ts with unlimited ustomization possibilities. Pixel-perfect replication of the designers is intended.</p>
+                        <p>Brook presents your services with flexible, convefnient and chient anipurpose layouts. You can select your favorite layouts.</p>
+                        <a href="about.html" class="btn">More About Us</a>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12">
+                    <!-- about-img -->
+                    <div class="about-img ">
+                        <div class="about-font-img">
+                            <img src="assets/img/gallery/about2.png" alt="">
+                        </div>
+                        <div class="about-back-img d-none d-lg-block">
+                            <img src="assets/img/gallery/about1.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- About Area End -->
+    <!--? contact-form start -->
+    <section class="contact-form-area section-bg  pt-115 pb-120 fix" data-background="assets/img/gallery/section_bg02.jpg">
+        <div class="container">
+            <div class="row justify-content-end">
+                <!-- Contact wrapper -->
+                <div class="col-xl-8 col-lg-9">
+                    <div class="contact-form-wrapper">
+                        <!-- From tittle -->
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <!-- Section Tittle -->
+                                <div class="section-tittle mb-50">
+                                    <span>Get a Qote For Free</span>
+                                    <h2>Request a Free Quote</h2>
+                                    <p>Brook presents your services with flexible, convenient and cdpose layouts. You can select your favorite layouts & elements for.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- form -->
+                        <form action="#" class="contact-form">
+                            <div class="row ">
+                                <div class="col-lg-6 col-md-6">
+                                    <div class="input-form">
+                                        <input type="text" placeholder="Name">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6">
+                                    <div class="input-form">
+                                        <input type="text" placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="input-form">
+                                        <input type="text" placeholder="Contact Number">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="select-items">
+                                        <select name="select" id="select1">
+                                            <option value="">Freight Type</option>
+                                            <option value="">Catagories One</option>
+                                            <option value="">Catagories Two</option>
+                                            <option value="">Catagories Three</option>
+                                            <option value="">Catagories Four</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6">
+                                    <div class="input-form">
+                                        <input type="text" placeholder="City of Departure">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-6">
+                                    <div class="input-form">
+                                        <input type="text" placeholder="Incoterms">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-6">
+                                    <div class="input-form">
+                                        <input type="text" placeholder="Weight">
+                                    </div>
+                                </div>
+                                <!-- Height Width length -->
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="input-form">
+                                        <input type="text" placeholder="Height">
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="input-form">
+                                        <input type="text" placeholder="Width">
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-6 col-sm-6">
+                                    <div class="input-form">
+                                        <input type="text" placeholder="length">
+                                    </div>
+                                </div>
+                                <!-- Radio Button -->
+                                <div class="col-lg-12">
+                                    <div class="radio-wrapper mb-30 mt-15">
+                                        <label>Extra services:</label>
+                                        <div class="select-radio">
+                                            <div class="radio">
+                                                <input id="radio-1" name="radio" type="radio" checked="">
+                                                <label for="radio-1" class="radio-label">Freight</label>
+                                            </div>
+                                            <div class="radio">
+                                                <input id="radio-2" name="radio" type="radio">
+                                                <label for="radio-2" class="radio-label">Express Delivery</label>
+                                            </div>
+                                            <div class="radio">
+                                                <input id="radio-4" name="radio" type="radio">
+                                                <label for="radio-4" class="radio-label">Insurance</label>
+                                            </div>
+                                            <div class="radio">
+                                                <input id="radio-5" name="radio" type="radio">
+                                                <label for="radio-5" class="radio-label">Packaging</label>
+                                            </div>
+                                        </div>
+                                    </div> 
+                                </div>
+                                <!-- Button -->
+                                <div class="col-lg-12">
+                                    <button name="submit" class="submit-btn">Request a Quote</button>
+                                </div>
+                            </div>
+                        </form>	
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- contact-form end -->
+</main>
+
+<?php include ('./components/footer.php'); ?>
